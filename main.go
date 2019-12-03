8104e4da434645d52eebab8ce0a93a29bb0a1dab
wilsonwaterman <32527676+wilsonwaterman@users.noreply.github.com>
2019-12-03 16:15:33 -0500

    Update Main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "New Message"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
